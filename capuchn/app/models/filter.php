94cebf9fed1d3a2545162f8fd6030dbe9d3655c8
stillboy <stillboy@a93da39f-844c-0410-829b-f9aeadadb08b>
2008-05-15 20:47:20 +0000

    Many bugfixes and code changes, created the theme controller and a filter controller. Deleted old and unused files.

---
+++ capuchn/app/models/filter.php
@@ -0,0 +1,18 @@
+<?php
+class Filter extends AppModel
+{
+    var $name = 'Filter';
+	
+	/*
+	 * Filter is structured like
+	 * id - id
+	 * name - reference name
+	 * text - regex expression
+	 * type php|text - type of action
+	 * code - the code to replace with or action
+	 * active - boolean, is active
+	 */
+	
+	
+}
+?>
